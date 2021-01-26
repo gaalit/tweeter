a5ca2d8a66dc293d6c185cb89c6f61a8f8ab0cf2
Galit <galit.gerasimov3@gmail.com>
2021-01-26 22:03:23 +0000

    Refactored

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
   $("#tweet-text").on('keyup', function(event) {
-  const text = event.target.value;
+  const text = this.value.length;
   const limit = 140;
-  const updatedCounter = limit - text.length;
+  const updatedCounter = limit - text;
   $(".counter").html(updatedCounter);
 
   if(updatedCounter < 0) {
