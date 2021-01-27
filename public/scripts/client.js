7908e91aeb48a72ee316a2a139d5b4b424ed795d
Galit <galit.gerasimov3@gmail.com>
2021-01-27 19:46:48 +0000

    Created functions to make tweets dynamic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function (tweetData) {
+  let $tweet = $("article").addClass("tweet")
+  
+  let html = 
+  `<header>
+    <div class="user-info">
+      <img src=${tweetData.user.avatars}> 
+      <h1>${tweetData.user.name}</h1>
+     </div>
+     <div>
+      <h2>${tweetData.user.handle}</h2>
+     </div>
+  </header>
+   <div class="tweet-text">
+    <p>
+      ${tweetData.content.text}
+    </p>
+  </div>
+  <footer>
+    <p>
+      ${tweetData.created_at}
+      </p>
+  </footer>
+  `
+  let result = $tweet.append(html);
+  return result;
+}
+
+const renderTweets = function(tweets) {
+  
+  const container = $(".tweet-container").html("");
+  tweets.forEach((tweet) => {
+    const tweetElement = createTweetElement(tweet)
+    container.prepend(tweetElement);
+  })
+}
+
